228d8a02595951c4b50ab7b02f2d0d3059596a24
bencrow11 <bencrow11@gmail.com>
2023-08-17 18:29:19 +0100

    Added extra config options for automatic tournaments.

--- common/src/main/java/org/pokesplash/cobblemontournaments/config/MainConfig.java
+++ common/src/main/java/org/pokesplash/cobblemontournaments/config/MainConfig.java
@@ -14,6 +14,14 @@ public class MainConfig {
 	private final String webhook_url;
 	// Presets for the polls.
 	private final ArrayList<String> poll_presets;
+	// If polls should be automatically scheduled after a tournament finishes.
+	private final boolean automatic_polls;
+	// The duration in hours that the poll should be open for.
+	private final int poll_duration;
+	// The duration in days between tournaments voted for by polls.
+	private final int poll_tournament_duration;
+	// Automatic tournament sign up period, in minutes.
+	private final int signup_duration;
 
 	/**
 	 * Constructor to create a config with default values.
@@ -24,6 +32,10 @@ public class MainConfig {
 		webhook_url = "ENTER_URL_HERE";
 		poll_presets = new ArrayList<>();
 		poll_presets.add("ExamplePreset");
+		automatic_polls = true;
+		poll_duration = 24;
+		poll_tournament_duration = 28;
+		signup_duration = 10;
 	}
 
 	/**
@@ -58,6 +70,35 @@ public class MainConfig {
 		return poll_presets;
 	}
 
+	/**
+	 * Getter for if polls are automatically generated.
+	 * @return true if the poll is automatic.
+	 */
+	public boolean isAutomaticPolls() {
+		return automatic_polls;
+	}
 
+	/**
+	 * Getter for the duration the polls last.
+	 * @return the duration in hours as an int.
+	 */
+	public int getPollDuration() {
+		return poll_duration;
+	}
 
+	/**
+	 * Getter for the duration between tournaments voted for on polls.
+	 * @return duration in days between tournaments.
+	 */
+	public int getTournamentDuration() {
+		return poll_tournament_duration;
+	}
+
+	/**
+	 * Getter for the signup duration of automatic tournaments.
+	 * @return the signup duration in minutes.
+	 */
+	public int getSignupDuration() {
+		return signup_duration;
+	}
 }
